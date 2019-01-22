e0e530ec23a6b7ed1cf8c6f80cf302ba26cf26c8
Ans de Nijs <ans.de.nijs@frontmen.nl>
2019-01-22 16:52:13 +0100

    Local storage added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+import './index.css'
+import { loadState, saveState } from './localStorage.js'
+import throttle from 'lodash/throttle'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const persistedState = loadState()
+
+const store = createStore(
+    todoApp,
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+store.subscribe(
+    throttle(() => {
+        saveState({
+            todos: store.getState().todos,
+        })
+    }, 1000)
+)
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
