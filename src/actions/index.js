cd385eb3cbc1138e88ca4e76be3678414f37c7c6
Ans de Nijs <ans.de.nijs@frontmen.nl>
2019-01-30 09:48:41 +0100

    Data on the server + normalized API responses

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { normalize } from 'normalizr';
+import * as schema from './schema';
 import * as api from '../api';
 import { getIsFetching } from '../reducers/createList';
 
@@ -14,7 +16,7 @@ export const fetchTodos = filter => (dispatch, getState) => {
             dispatch({
                 type: 'FETCH_TODOS_SUCCESS',
                 filter,
-                response,
+                response: normalize(response, schema.arrayOfTodos),
             });
         },
         error => {
@@ -31,14 +33,17 @@ export const addTodo = text => dispatch =>
     api.addTodo(text).then(response => {
         dispatch({
             type: 'ADD_TODO_SUCCESS',
-            response,
+            response: normalize(response, schema.todo),
         });
     });
 
-export const toggleTodo = id => ({
-    type: 'TOGGLE_TODO',
-    id,
-});
+export const toggleTodo = id => dispatch =>
+    api.toggleTodo(id).then(response => {
+        dispatch({
+            type: 'TOGGLE_TODO_SUCCESS',
+            response: normalize(response, schema.todo),
+        });
+    });
 
 export const VisibilityFilters = {
     SHOW_ALL: 'SHOW_ALL',
